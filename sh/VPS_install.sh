c5f41a744c1e7c2a372941b37bb78bc8372087e2
chaonimei <102893132+chaonimei@users.noreply.github.com>
2023-01-06 19:43:54 +0800

    Update VPS_install.sh

--- sh/VPS_install.sh
+++ sh/VPS_install.sh
@@ -4,7 +4,5 @@ su
 apt update -y
 apt install screen unzip zip curl w3m wget openssl sudo -y
 cd /root
-mkdir downloads
-mkdir curl
-screen -y
+screen
 wget -O install.sh https://download.bt.cn/install/install-ubuntu_6.0.sh && sudo bash install.sh ed8484bec
